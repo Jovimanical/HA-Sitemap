48b95ead66d84957e29c9b7e4c1e68aeaea5df73
S.A.V.I.O.U.R <S.A.V.I.O.U.R@SIMILLUSTRA.COM>
2022-04-12 13:55:02 +0100

    initial commit-19g

--- scripts/install_composer_dependencies.sh
+++ scripts/install_composer_dependencies.sh
@@ -34,9 +34,8 @@ sudo curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 sudo curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
 sudo wget https://packages.microsoft.com/config/ubuntu/19.10/prod.list
 sudo mv prod.list /etc/apt/sources.list.d/mssql-release.list
-#sudo apt-get update
-#sudo apt-get install mssql-tools unixodbc-dev
 sudo curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
+
 echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
 sudo apt-get update -yq --allow-unauthenticated --allow-insecure-repositories && sudo ACCEPT_EULA=Y apt-get install -yq libodbc1 unixodbc mssql-tools unixodbc-dev  --allow-unauthenticated
 
