6a6a24b06ea4b7b0037290f8ec907b5a8c865398
S.A.V.I.O.U.R <S.A.V.I.O.U.R@SIMILLUSTRA.COM>
2022-04-12 09:28:09 +0100

    initial commit-9

--- scripts/install_composer_dependencies.sh
+++ scripts/install_composer_dependencies.sh
@@ -21,10 +21,21 @@ php composer.phar install
 
 sudo wget http://archive.ubuntu.com/ubuntu/pool/main/g/glibc/multiarch-support_2.27-3ubuntu1.5_amd64.deb
 sudo apt-get install ./multiarch-support_2.27-3ubuntu1.5_amd64.deb
-sudo curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
-sudo apt-get update -y --allow-unauthenticated --allow-insecure-repositories && sudo ACCEPT_EULA=Y apt-get install -yq libodbc1 unixodbc msodbcsql17 mssql-tools unixodbc-dev  --allow-unauthenticated
 
 
+sudo apt-get install tdsodbc -y
+sudo curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+sudo curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
+sudo wget https://packages.microsoft.com/config/ubuntu/19.10/prod.list
+sudo mv prod.list /etc/apt/sources.list.d/mssql-release.list
+#sudo apt-get update
+#sudo apt-get install mssql-tools unixodbc-dev
+#sudo curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
+sudo apt-get update -y --allow-unauthenticated --allow-insecure-repositories && sudo ACCEPT_EULA=Y apt-get install -yq libodbc1 unixodbc mssql-tools unixodbc-dev  --allow-unauthenticated
+
+#echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile
+#echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
+#source ~/.bashrc
 
 sudo chown -R www-data:www-data /var/www/html
 sudo chmod -R 0777 /var/www/html
