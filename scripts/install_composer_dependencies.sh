0444d1c32076186400829eb79490b34da09b18e3
S.A.V.I.O.U.R <S.A.V.I.O.U.R@SIMILLUSTRA.COM>
2022-04-12 10:25:36 +0100

    initial commit-17

--- scripts/install_composer_dependencies.sh
+++ scripts/install_composer_dependencies.sh
@@ -22,17 +22,17 @@ php composer.phar install
 #sudo wget http://archive.ubuntu.com/ubuntu/pool/main/g/glibc/multiarch-support_2.27-3ubuntu1.5_amd64.deb
 #sudo apt-get install ./multiarch-support_2.27-3ubuntu1.5_amd64.deb
 
-export APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=1
-sudo apt-get install tdsodbc -y
+#export APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=1
+#sudo apt-get install tdsodbc -y
 #sudo curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-sudo curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
+#sudo curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
 #sudo wget https://packages.microsoft.com/config/ubuntu/19.10/prod.list
 #sudo mv prod.list /etc/apt/sources.list.d/mssql-release.list
 #sudo apt-get update
 #sudo apt-get install mssql-tools unixodbc-dev
 #sudo curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
-echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
-sudo apt-get update -yq --allow-unauthenticated --allow-insecure-repositories && sudo ACCEPT_EULA=Y apt-get install -yq libodbc1 unixodbc mssql-tools unixodbc-dev  --allow-unauthenticated
+#echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
+#sudo apt-get update -yq --allow-unauthenticated --allow-insecure-repositories && sudo ACCEPT_EULA=Y apt-get install -yq libodbc1 unixodbc mssql-tools unixodbc-dev  --allow-unauthenticated
 
 #echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile
 #echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
@@ -42,11 +42,3 @@ sudo chown -R www-data:www-data /var/www/html
 sudo chmod -R 0777 /var/www/html
 
 sudo systemctl reload apache2
-
-
-# Add cronJobs to Service
-#These are piped through the sort command to remove duplicate lines.
-#but we can achieve a less destructive de-duplication with awk:
-#cd ~
-#(crontab -l; echo "* * * * * /usr/bin/php /var/www/html/cronJobs/index.php")|awk '!x[$0]++'|crontab -
-#(crontab -l; echo "* * * * * root /usr/bin/wget -O - http://localhost/cronJobs/index.php")|awk '!x[$0]++'|crontab -
