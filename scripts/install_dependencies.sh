e7e442c6797b4c3ccc541e7c7de935505b9b001e
S.A.V.I.O.U.R <S.A.V.I.O.U.R@SIMILLUSTRA.COM>
2022-04-16 14:24:58 +0100

    initial commit-31 patched permission

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -23,6 +23,9 @@ if ! [ -x "$(command -v apache2)" ]; then
   sudo apt install -y php7.4 php7.4-zip php7.4-dev php-pear libapache2-mod-php7.4 php7.4-gmp php7.4-mysql php7.4-gd php7.4-xml php7.4-soap php7.4-mbstring php7.4-mysql php7.4-redis php7.4-curl php7.4-cli php7.4-zip php7.4-yaml php7.4-common php7.4-bcmath php7.4-json
 
   sudo pecl install sqlsrv && sudo pecl install pdo_sqlsrv
+  #updating Write permission
+  sudo chmod a+rwx /etc/php/7.4/mods-available/sqlsrv.ini
+  sudo chmod a+rwx /etc/php/7.4/mods-available/pdo_sqlsrv.ini
 
   sudo -i
   printf "; priority=20\nextension=sqlsrv.so\n" >/etc/php/7.4/mods-available/sqlsrv.ini
